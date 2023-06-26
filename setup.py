106df5644a97f5ce94e53cb5c2d71769defc4f7d
Sean Brynjolfsson <smb459@cornell.edu>
2023-06-26 10:53:19 -0400

    Change module name from StageBuilder -> stage_builder

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 setup(
     name='StageBuilder',
-    packages=find_packages(include=['StageBuilder']),
+    packages=find_packages(include=['stage_builder']),
     version='0.0.1',
     description='Temp description',
     author='Sean Brynjolfsson',
