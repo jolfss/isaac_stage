7620288f7133b29db77128c89e8d1dfa87c74d40
Sean Brynjolfsson <smb459@cornell.edu>
2023-06-26 10:55:04 -0400

    Make sure package name is stage_builder.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 setup(
-    name='StageBuilder',
+    name='stage_builder',
     packages=find_packages(include=['stage_builder']),
     version='0.0.1',
     description='Temp description',
